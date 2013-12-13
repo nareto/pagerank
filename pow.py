2e03503ab1409647706646ada52e84a26e851fab
jboogie <rennabh@gmail.com>
2013-12-13 13:53:06 +0100

    esthetic change

--- pow.py
+++ pow.py
@@ -6,7 +6,7 @@ def usage(name):
     print "Applies power method to PageRank problem. You have to supply the P matrix as a .npy file and an outfile to write residues"
     print "USAGE: python {0} pmatrix.npy outfile".format(name)
 
-def pow(P,outfile,alpha=0.85,tol=1.e-5,maxiter=300):
+def pow(P,outfile,alpha=0.85,tol=1.e-6,maxiter=300):
     file = open(outfile,'w')
     n = float(P.shape[0])
     b = (1-alpha)*(1/n)*np.ones(n)
@@ -14,16 +14,13 @@ def pow(P,outfile,alpha=0.85,tol=1.e-5,maxiter=300):
     res = np.array([1.0])
     iter = 0
     while res[-1] > tol and iter < maxiter:
-        dot = P.dot(x)
-        y = alpha*dot
-
-        y = y + b
+        y = (alpha*P.dot(x)) + b
         y = y/np.linalg.norm(y)
         res = np.append(res,np.linalg.norm(x-y))
         x = y
         iter += 1
         
-    np.savetxt(file,res)
+    np.savetxt(file,res[1:])
     file.close()
 
 if __name__ == "__main__":
