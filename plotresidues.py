5d97a02c3f61121b4583873eae61a3d9667baa51
jboogie <rennabh@gmail.com>
2013-12-15 12:53:02 +0100

    added residues plot

---
+++ plotresidues.py
@@ -0,0 +1,17 @@
+import matplotlib.pyplot as plt
+import numpy as np
+from pow import pow
+from innerouter import innerouter
+
+matrix = "harvard500.npy"
+a = 0.85
+P=np.load(matrix)
+xpow,respow = pow(P, alpha=a)
+xinout,resinout = innerouter(P, alpha = a)
+
+plt.plot(respow,'.g', resinout, '.r')
+plt.yscale('log')
+plt.legend(('power method','inner-outer'))
+#plt.text(20.0,-1.0,'Residues',fontsize=24)#, bbox=dict(edgecolor = 'black', facecolor='white'))
+plt.suptitle(r'Residues for $\alpha = %.2f$' % a,y=0.8,fontsize=24)
+plt.show()
