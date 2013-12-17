fa98e94f7471de28ebca1ccc33448dedb73208b4
jboogie <rennabh@gmail.com>
2013-12-17 20:58:13 +0100

    changed to norm 1

--- plotresidues.py
+++ plotresidues.py
@@ -16,9 +16,10 @@ a = 0.85
 #or better load residue vector from previous computtation
 respow = np.loadtxt(basename+"-pow-residues")
 resinout = np.loadtxt(basename+"-inout-residues")
+#resdirsample = np.loadtxt(basename+"-dirsample-residues")
 plt.plot(respow,'.g', resinout, '.r')
 plt.yscale('log')
-plt.legend(('power method','inner-outer'))
+plt.legend(('power method','inner-outer', 'direct sampling'))
 #plt.text(20.0,-1.0,'Residues',fontsize=24)#, bbox=dict(edgecolor = 'black', facecolor='white'))
 plt.suptitle(r'Residues for $\alpha = %.2f$' % a,y=0.8,fontsize=24)
 plt.show()
